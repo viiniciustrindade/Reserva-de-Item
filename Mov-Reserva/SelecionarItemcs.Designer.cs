34c1336269aad7b55481af2cab2477e52684e4ef
Vinicius Trindade <vinicius.mjt@gmail.com>
2023-05-24 12:00:20 -0300

    no message

--- Mov-Reserva/SelecionarItemcs.Designer.cs
+++ Mov-Reserva/SelecionarItemcs.Designer.cs
@@ -28,12 +28,312 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtExemplar = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCodItem = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLocalizacao = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTipoItem = new System.Windows.Forms.TextBox();
+            this.txtNomeItem = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dadosGrid = new System.Windows.Forms.DataGridView();
+            this.colCodItem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNomeItem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCodVolume = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNumExemplar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colColecao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLocal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEdicao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAutor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLocalizacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEditora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTipoItem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSecao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dadosGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtExemplar
+            // 
+            this.txtExemplar.Location = new System.Drawing.Point(95, 40);
+            this.txtExemplar.Name = "txtExemplar";
+            this.txtExemplar.Size = new System.Drawing.Size(88, 22);
+            this.txtExemplar.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Exemplar";
+            // 
+            // txtCodItem
+            // 
+            this.txtCodItem.Location = new System.Drawing.Point(95, 12);
+            this.txtCodItem.Name = "txtCodItem";
+            this.txtCodItem.Size = new System.Drawing.Size(88, 22);
+            this.txtCodItem.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Codigo item";
+            // 
+            // txtLocalizacao
+            // 
+            this.txtLocalizacao.Location = new System.Drawing.Point(480, 40);
+            this.txtLocalizacao.Name = "txtLocalizacao";
+            this.txtLocalizacao.Size = new System.Drawing.Size(92, 22);
+            this.txtLocalizacao.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(394, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Localização";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Tipo item";
+            // 
+            // txtTipoItem
+            // 
+            this.txtTipoItem.Location = new System.Drawing.Point(271, 40);
+            this.txtTipoItem.Name = "txtTipoItem";
+            this.txtTipoItem.Size = new System.Drawing.Size(117, 22);
+            this.txtTipoItem.TabIndex = 15;
+            // 
+            // txtNomeItem
+            // 
+            this.txtNomeItem.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNomeItem.Location = new System.Drawing.Point(271, 12);
+            this.txtNomeItem.Name = "txtNomeItem";
+            this.txtNomeItem.Size = new System.Drawing.Size(301, 22);
+            this.txtNomeItem.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Nome item";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Selecionar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dadosGrid
+            // 
+            this.dadosGrid.AllowUserToAddRows = false;
+            this.dadosGrid.AllowUserToDeleteRows = false;
+            this.dadosGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dadosGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dadosGrid.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dadosGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dadosGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCodItem,
+            this.colNomeItem,
+            this.colCodVolume,
+            this.colNumExemplar,
+            this.colColecao,
+            this.colLocal,
+            this.colEdicao,
+            this.colAutor,
+            this.colLocalizacao,
+            this.colStatus,
+            this.colEditora,
+            this.colTipoItem,
+            this.colSecao});
+            this.dadosGrid.Location = new System.Drawing.Point(13, 98);
+            this.dadosGrid.Margin = new System.Windows.Forms.Padding(4);
+            this.dadosGrid.Name = "dadosGrid";
+            this.dadosGrid.ReadOnly = true;
+            this.dadosGrid.RowHeadersWidth = 51;
+            this.dadosGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dadosGrid.Size = new System.Drawing.Size(559, 274);
+            this.dadosGrid.TabIndex = 27;
+            // 
+            // colCodItem
+            // 
+            this.colCodItem.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.colCodItem.FillWeight = 152.2843F;
+            this.colCodItem.HeaderText = "Codigo";
+            this.colCodItem.MinimumWidth = 6;
+            this.colCodItem.Name = "colCodItem";
+            this.colCodItem.ReadOnly = true;
+            this.colCodItem.Width = 80;
+            // 
+            // colNomeItem
+            // 
+            this.colNomeItem.HeaderText = "Livro";
+            this.colNomeItem.MinimumWidth = 6;
+            this.colNomeItem.Name = "colNomeItem";
+            this.colNomeItem.ReadOnly = true;
+            // 
+            // colCodVolume
+            // 
+            this.colCodVolume.FillWeight = 73.85786F;
+            this.colCodVolume.HeaderText = "Volume";
+            this.colCodVolume.MinimumWidth = 6;
+            this.colCodVolume.Name = "colCodVolume";
+            this.colCodVolume.ReadOnly = true;
+            this.colCodVolume.Visible = false;
+            // 
+            // colNumExemplar
+            // 
+            this.colNumExemplar.HeaderText = "Exemplar";
+            this.colNumExemplar.MinimumWidth = 6;
+            this.colNumExemplar.Name = "colNumExemplar";
+            this.colNumExemplar.ReadOnly = true;
+            this.colNumExemplar.Visible = false;
+            // 
+            // colColecao
+            // 
+            this.colColecao.HeaderText = "Coleção";
+            this.colColecao.MinimumWidth = 6;
+            this.colColecao.Name = "colColecao";
+            this.colColecao.ReadOnly = true;
+            this.colColecao.Visible = false;
+            // 
+            // colLocal
+            // 
+            this.colLocal.HeaderText = "Local";
+            this.colLocal.MinimumWidth = 6;
+            this.colLocal.Name = "colLocal";
+            this.colLocal.ReadOnly = true;
+            // 
+            // colEdicao
+            // 
+            this.colEdicao.HeaderText = "Ano de Edição";
+            this.colEdicao.MinimumWidth = 6;
+            this.colEdicao.Name = "colEdicao";
+            this.colEdicao.ReadOnly = true;
+            this.colEdicao.Visible = false;
+            // 
+            // colAutor
+            // 
+            this.colAutor.HeaderText = "Autor";
+            this.colAutor.MinimumWidth = 6;
+            this.colAutor.Name = "colAutor";
+            this.colAutor.ReadOnly = true;
+            // 
+            // colLocalizacao
+            // 
+            this.colLocalizacao.HeaderText = "Localização";
+            this.colLocalizacao.MinimumWidth = 6;
+            this.colLocalizacao.Name = "colLocalizacao";
+            this.colLocalizacao.ReadOnly = true;
+            this.colLocalizacao.Visible = false;
+            // 
+            // colStatus
+            // 
+            this.colStatus.HeaderText = "Status";
+            this.colStatus.MinimumWidth = 6;
+            this.colStatus.Name = "colStatus";
+            this.colStatus.ReadOnly = true;
+            // 
+            // colEditora
+            // 
+            this.colEditora.HeaderText = "Editora";
+            this.colEditora.MinimumWidth = 6;
+            this.colEditora.Name = "colEditora";
+            this.colEditora.ReadOnly = true;
+            // 
+            // colTipoItem
+            // 
+            this.colTipoItem.HeaderText = "Tipo Item";
+            this.colTipoItem.MinimumWidth = 6;
+            this.colTipoItem.Name = "colTipoItem";
+            this.colTipoItem.ReadOnly = true;
+            this.colTipoItem.Visible = false;
+            // 
+            // colSecao
+            // 
+            this.colSecao.HeaderText = "Seção";
+            this.colSecao.MinimumWidth = 6;
+            this.colSecao.Name = "colSecao";
+            this.colSecao.ReadOnly = true;
+            // 
+            // SelecionarItemcs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SelecionarItemcs";
+            this.ClientSize = new System.Drawing.Size(584, 385);
+            this.Controls.Add(this.dadosGrid);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLocalizacao);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTipoItem);
+            this.Controls.Add(this.txtNomeItem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtExemplar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCodItem);
+            this.Controls.Add(this.label2);
+            this.Name = "SelecionarItemcs";
+            this.Text = "Selecionar item";
+            ((System.ComponentModel.ISupportInitialize)(this.dadosGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtExemplar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCodItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLocalizacao;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTipoItem;
+        private System.Windows.Forms.TextBox txtNomeItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dadosGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCodItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNomeItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCodVolume;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNumExemplar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colColecao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLocal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEdicao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAutor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLocalizacao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colStatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEditora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTipoItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSecao;
     }
 }
